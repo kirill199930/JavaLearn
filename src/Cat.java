8973ecfdbdbb04ebc2fe5f3ba574f4e4ca3c1b5c
user <kirillhannanovgit274@gmail.com>
2022-06-24 20:36:00 +0500

    JavaLearn

---
+++ src/Cat.java
@@ -0,0 +1,83 @@
+
+public class Cat
+{
+    private double originWeight;
+    private double weight;
+    private static int count;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        count++;
+
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        int sum = 0;
+        sum += amount;
+        System.out.println("sumEat " + sum);
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void dead(){
+        while (true){
+            if(weight >= minWeight) meow();
+            else break;
+        }
+    }
+
+
+    public void pee(){
+        weight -= 10;
+        System.out.println(weight);
+    }
+
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public static int countCat(){
+        return count;
+    }
+    public void deadCat(){
+        if (weight < minWeight) count--;
+        else if (weight > maxWeight  ) count--;
+
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
